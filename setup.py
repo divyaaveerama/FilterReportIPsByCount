8e208378f1ac8f693a9b70432abf9d729837ae2d
divyaaveerama <47803361+divyaaveerama@users.noreply.github.com>
2020-11-19 15:28:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="FilterReportIPsByCount.py", 
+    name="FilterReportIPsByCount-divyaaveerama", 
     version="1.0.0",
     author="Divyaa Kamalanathan",
     author_email="divyaa.kamalanathan@intrinium.com",
