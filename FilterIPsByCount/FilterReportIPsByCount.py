36668b808f25539d23012755991d0057cbdd20c3
divyaaveerama <47803361+divyaaveerama@users.noreply.github.com>
2020-11-19 19:36:02 -0800

    Update FilterReportIPsByCount.py

--- FilterIPsByCount/FilterReportIPsByCount.py
+++ FilterIPsByCount/FilterReportIPsByCount.py
@@ -1,12 +1,28 @@
+# FilterReportIPsByCount
+# Divyaa Kamalanathan 2020
+# Script to read through a report on an excel file, grab IPs if it is greater than or equal to a set count and check if malicious
+
+
+import sys
 import requests
 import xlrd
 import re
+import getopt
+import config
 
-def getIPsFromExcelFile():
+#Function to check the Excel file for IP addresses and add those to a list
+
+def getIPsFromExcelFile(filename):
     IPs = []
     IPCheck = re.compile(r'^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)$')
-    excelFile = (r"C:\Users\dkamalanathan\OneDrive - Intrinium\Documents\FilterIPsByCount\Children Hospital LA X-force Scanning IPs Categorization.xls")
-    wb = xlrd.open_workbook(excelFile)
+    excelFile = (filename)
+    
+    try:
+        wb = xlrd.open_workbook(excelFile)
+    except:
+        print("Input File not Found")
+        sys.exit(2)
+        
     sheet = wb.sheet_by_index(0)
     for x in range(sheet.ncols - 1):
         for y in range(sheet.nrows - 1):
@@ -16,13 +32,10 @@ def getIPsFromExcelFile():
     return IPs
 
 
-def getIPsToBan(IPs):
-    # IPs = []
+#Check list of IPs to see if each IP appears <set count> or more times 
+def getIPsToBan(IPs,count):
     IPsWithCount = {}
     IPsToBan = []
-
-    # fo = open("../IPList.txt", "r")
-    # IPs = fo.readlines()
 
     for IP in IPs:
         if IP in IPsWithCount:
@@ -32,30 +45,85 @@ def getIPsToBan(IPs):
             IPsWithCount[IP] = 1
 
     for IP in IPsWithCount:
-        if IPsWithCount[IP] >= 10:
+        if IPsWithCount[IP] >= count:
             IPsToBan.append(IP)
             
     return IPsToBan
 
+#Use IPVoid API to check if IP has been blacklisted, returns IPs and IP Information
 def checkIfMalicious(IPsToCheck):
-    apikey = "c59c8a2ac5998fce2a6d259cc431bb31b5485fef"
-    MalIPs = []
+    apikey = config.api_key
+    MalIPs = {}
     for IP in IPsToCheck:
         response = requests.get('https://endpoint.apivoid.com/iprep/v1/pay-as-you-go/?key='+apikey+'&ip='+IP)
         if response.status_code == 200:
             response_json = response.json()
             try:
                 if response_json['data']['report']['blacklists']['detections'] >= 1:
-                    MalIPs.append(IP)
+                    blacklistStatus = "Blacklisted " + str(response_json['data']['report']['blacklists']['detections']) +"/" + str(response_json['data']['report']['blacklists']['engines_count'])
+                    reverseDNS = str(response_json['data']['report']['information']['reverse_dns'])
+                    ISP = str(response_json['data']['report']['information']['isp'])
+                    Continent = str(response_json['data']['report']['information']['continent_name'])
+                    Country = str(response_json['data']['report']['information']['country_name'])
+                    analysis = "IP Address : " + IP + "\nBlacklist Status: " + blacklistStatus + "\nReverse DNS: " +reverseDNS+ "\nISP: " +ISP+ "\nContinent: " + Continent + "\nCountry: " + Country + "\n\n"
+                    MalIPs[IP] = analysis
             except:
                 pass
                 
     return MalIPs
 
-if __name__ == "__main__":
-    import sys
-    IPs = getIPsFromExcelFile()
+# Print IPs to ban in a list and IP info to file
+def PrintIPInformation(maliciousIPs,filename):
+    fo = open(filename,"w+)
+    fo.write(maliciousIPs.keys())
+    fo.write("\n")
+    fo.write(maliciousIPs.values())
+
+# Print IPs to ban in a list and IP info to stdout, if no output file is specified
+def PrintIPInformation(maliciousIPs):
+    print(maliciousIPs.keys())
+    print("\n")
+    print(maliciousIPs.values())
+
+#Main Function    
+def main(argv):
+    excelFileName = ''
+    outputFileName = ''
+    count = 0
+    
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:c:")
+        
+    except getopt.GetoptError:
+        print("FilterReportIPsByCount.py -i <input filename> -o <output filename> -c <count of IPs to check for>")
+        sys.exit(2)
+        
+    for opt, arg in opts:
+        if opt == '-h':
+             print("FilterReportIPsByCount.py -i <input filename> -o <output filename> -c <count of IPs to check for>")
+             sys.exit()
+             
+        elif opt in ("-i"):
+            excelFileName = arg
+            
+        elif opt in ("-o"):
+           outputFileName = arg
+        
+        elif opt in ("-c"):
+            count = int(arg)
+            
+    if count <= 0:
+        print("Invalid count")
+        sys.exit(2)
+        
+    IPs = getIPsFromExcelFile(excelFileName)
     IPsToBan = getIPsToBan(IPs)
     MaliciousIPs = checkIfMalicious(IPsToBan)
-    for IP in MaliciousIPs:
-        print(IP)
+    if outputFileName != '':
+        PrintIPInformation(MaliciousIPs,outputFileName)
+    else:
+        PrintIPInformation(MaliciousIPs)
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
