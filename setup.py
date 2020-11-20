ddff558d016fddebf68f62a5bca82ba70bddfb2f
divyaaveerama <47803361+divyaaveerama@users.noreply.github.com>
2020-11-20 01:25:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="FilterReportIPsByCount-divyaaveerama", 
+    name="FilterReportIPsByCount", 
     version="1.0.1",
     author="Divyaa Kamalanathan",
     author_email="divyaa.kamalanathan@intrinium.com",
-    description="Script to read through a report, grab IPs and check if malicious",
+    description="Script to read through a report, grab IPs and check if malicious, output-ing malicious IP information",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/divyaaveerama/FilterReportIPsByCount",
